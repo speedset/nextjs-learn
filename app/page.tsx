f17d540ab2572eeff95236f005d4b71861f78cf3
speedset <garciadaniloadrian@gmail.com>
2024-01-07 17:22:56 -0300

    nuevo1

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import styles from '@/app/ui/home.module.css';
 import clsx from 'clsx';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-export default async function Page({ status }: { status: string }) {
+export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
